8b06aa23f45cf5e4baa82d89b75b05cb6cdd4197
Dominic Doyle <andrewdoyle@Dominics-MacBook-Air.local>
2021-05-26 16:21:02 +0100

    updated double letter with alt solutions

--- solutions/double_letter.py
+++ solutions/double_letter.py
@@ -28,6 +28,25 @@ def double_letter(wd):
 	return False 
 
 
+def double_letters2(string):
+    x = 0
+    while x < len(string) - 1:  
+        if string[x] == string[x+1]:
+            return True         
+        x = x+1
+    return False
+
+def double_letter_w_range(string):
+    for x in range(len(string)):  
+        if string[x] == string[x+1]:
+            return True 
+    return False
+
+def find_double_letters3(word):
+    return len([letter for letter in word if letter*2 in word]) >= 1
+
+
+
 if __name__ == '__main__':
 	print(double_letter("hello"))
 	print(double_letter("helo"))
